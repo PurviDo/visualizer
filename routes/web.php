f9fe243dde12bc58e6300184c830dea08322fc59
PurviDo <purvid.brainerhub@gmail.com>
2024-07-11 19:53:38 +0530

    modified route file

--- routes/web.php
+++ routes/web.php
@@ -9,18 +9,25 @@ use App\Http\Controllers\Admin\CustomerController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\SubCategoryController;
 
-// Route::get('/', [AuthController::class, 'index'])->name('login');
-// Route::get('/login', function () {
-//     return redirect(route('login'));
-// });
-Route::get('/', [AuthController::class, 'index'])->name('login');
-Route::post('/login', [AuthController::class, 'postLogin'])->name('login.post');
-Route::get('forget-password', [ForgotPasswordController::class, 'showForgetPasswordForm'])->name('forget.password.get');
-Route::post('forget-password', [ForgotPasswordController::class, 'submitForgetPasswordForm'])->name('forget.password.post');
-Route::get('reset-password/{token}', [ForgotPasswordController::class, 'showResetPasswordForm'])->name('reset.password.get');
-Route::post('reset-password', [ForgotPasswordController::class, 'submitResetPasswordForm'])->name('reset.password.post');
+Route::middleware('guest')->group(function () {
+    Route::get('/login', function () {
+        return view('admin.auth.login');
+    })->name('login');
 
+    Route::post('/login', [AuthController::class, 'postLogin'])->name('login.post');
+
+    Route::get('forget-password', [ForgotPasswordController::class, 'showForgetPasswordForm'])->name('forget.password.get');
+    Route::post('forget-password', [ForgotPasswordController::class, 'submitForgetPasswordForm'])->name('forget.password.post');
+
+    Route::get('reset-password/{token}', [ForgotPasswordController::class, 'showResetPasswordForm'])->name('reset.password.get');
+    Route::post('reset-password', [ForgotPasswordController::class, 'submitResetPasswordForm'])->name('reset.password.post');
+});
+
+// Authenticated routes
 Route::middleware('auth')->group(function () {
+    Route::get('/', function () {
+        return redirect()->route('dashboard');
+    });
 
     Route::get('logout', [AuthController::class, 'logout'])->name('logout');
     // Dashboard
