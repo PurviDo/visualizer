ee7828508fe03465415de6988665ceaa2d69e718
Purvi <purvid.brainerhub@gmail.com>
2024-07-26 13:28:15 +0530

    changes for ngrok

--- resources/views/admin/email-template/forgot-password.blade.php
+++ resources/views/admin/email-template/forgot-password.blade.php
@@ -1,4 +1,4 @@
 <h1>Forget Password Email</h1>
    
 You can reset password from bellow link:
-<a href="http://localhost:5173?token=<?php echo $token; ?>">Reset Password</a>
+<a href="https://camclo3d.vercel.app?token=<?php echo $token; ?>">Reset Password</a>
